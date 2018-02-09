6c2177ef0c32f23ec24eb60f9f882af5c3729290
Vince <vlin6@stuy.edu>
2018-02-09 14:41:18 -0500

    remove Queen working

--- 02/QueenBoard.java
+++ 02/QueenBoard.java
@@ -15,7 +15,7 @@ public class QueenBoard{
 	    for (int c = 0; c < board[r].length; c ++){
 		//System.out.println(board[r][c]);
 		if (board[r][c] == -1) System.out.print("Q ");
-		else System.out.print("_ ");
+		else System.out.print(board[r][c]);
 	    }
 	    System.out.print("\n");
 	}
@@ -37,9 +37,16 @@ public class QueenBoard{
 	return true;
     }
 
-    //   private boolean removeQueen(int r, int c){
-
-    //    }
+    private boolean removeQueen(int r, int c){
+	if (board[r][c] != -1) return false;
+	for (int ri = r; ri < board.length; ri ++){
+	    for (int ci = c; ci < board[ri].length; ci ++){
+	        if (ri == r || ci == c || ri - r == ci - c) board[ri][ci] --;
+	    }
+	}
+	board[r][c] += 2;
+	return true;
+    }
 
     public int countSolutions(){
 	
@@ -58,6 +65,7 @@ public class QueenBoard{
 	QueenBoard x = new QueenBoard(5);
 
 	x.addQueen(2,2);
+	x.removeQueen(2,2);
 	x.toString();
     }
 }
