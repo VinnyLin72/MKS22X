3b3ded1c1144932f02b2f5bc211454491726dd1a
“Vincent <vlin6@stuy.edu>
2018-02-11 22:07:27 -0500

    finished lab

--- 02/QueenBoard.java
+++ 02/QueenBoard.java
@@ -1,8 +1,13 @@
 public class QueenBoard{
     private int[][] board;
+    private int numSols;
 
     public QueenBoard(int size){
 	board = new int[size][size];
+	clear();
+    }
+
+    public void clear(){
 	for (int r = 0; r < board.length; r ++){
 	    for (int c = 0; c < board[r].length; c ++){
 		board[r][c] = 0;
@@ -10,27 +15,47 @@ public class QueenBoard{
 	}
     }
 
+    public void checkError(){
+	for (int r = 0; r < board.length; r++){
+	    for (int c = 0; c < board.length; c++){
+		if (board[r][c] != 0){
+		    throw new IllegalStateException();
+		}
+	    }
+	}
+    }
+    
     public String toString(){
 	for (int r = 0; r < board.length; r ++){
 	    for (int c = 0; c < board[r].length; c ++){
 		//System.out.println(board[r][c]);
 		if (board[r][c] == -1) System.out.print("Q ");
-		else System.out.print(board[r][c]);
+		else System.out.print("_ ");
 	    }
 	    System.out.print("\n");
 	}
 	return "";
     }
 
-    //   public boolean solve(){
-	
-    //  }
+    public boolean solve(){
+	checkError();
+	return solHelper(0);
+    }
+
+    public boolean solHelper(int c){
+	if (c >= board.length) return true;
+	for (int r = 0; r < board.length; r ++){
+	    if (addQueen(r, c) && solHelper(c + 1)) return true;
+	    removeQueen(r, c);
+	}
+    return false;
+    }
 
     public boolean addQueen(int r, int c){
 	if (board[r][c] != 0) return false;
 	for (int ri = r; ri < board.length; ri ++){
 	    for (int ci = c; ci < board[ri].length; ci ++){
-	        if (ri == r || ci == c || ri - r == ci - c) board[ri][ci] ++;
+	        if (ri == r || ci == c || Math.abs(ri - r) == Math.abs(ci - c)) board[ri][ci] ++;
 	    }
 	}
 	board[r][c] = -1;
@@ -41,7 +66,7 @@ public class QueenBoard{
 	if (board[r][c] != -1) return false;
 	for (int ri = r; ri < board.length; ri ++){
 	    for (int ci = c; ci < board[ri].length; ci ++){
-	        if (ri == r || ci == c || ri - r == ci - c) board[ri][ci] --;
+	        if (ri == r || ci == c ||  Math.abs(ri - r) == Math.abs(ci - c)) board[ri][ci] --;
 	    }
 	}
 	board[r][c] += 2;
@@ -49,23 +74,19 @@ public class QueenBoard{
     }
 
     public int countSolutions(){
-	
-	
-	int ans = 0;
-	for (int r = 0; r < board.length; r ++){
-	    for (int c = 0; c < board[r].length; c ++){
-		if (board[r][c] == -1) ans ++;
+	checkError();	
+	return countHelper(0);
+	    }
+
+    public int countHelper(int c){
+	if (c >= board.length) return numSols ++;
+	for(int r = 0;r < board.length;r++){
+	    if(addQueen(r,c)){
+		countHelper(c + 1);
+		removeQueen(r,c);
 	    }
 	}
-	return ans;
+	return numSols;
     }
-
-
-    public static void main(String[] args){
-	QueenBoard x = new QueenBoard(5);
-
-	x.addQueen(2,2);
-	x.removeQueen(2,2);
-	x.toString();
-    }
+    
 }
