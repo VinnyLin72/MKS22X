0bb51eed237b5cab1212d1ca7aa13014e099ef97
Vince <vlin6@stuy.edu>
2018-02-14 14:37:54 -0500

    stuff happened

--- 03/KnightBoard.java
+++ 03/KnightBoard.java
@@ -1,9 +1,9 @@
-public class KnightBoard(){
+public class KnightBoard{
 
     private int[][] board;
 
-    public KnightBoard(int r, int c){
-	board = new int[r][c];
+    public KnightBoard(int row, int col){
+	board = new int[row][col];
 	for (int r = 0; r < board.length; r ++){
 	    for (int c = 0; c < board[r].length; c ++){
 		board[r][c] = 0;
@@ -15,31 +15,32 @@ public class KnightBoard(){
 	String ans = "";
 	for (int r = 0; r < board.length; r ++){
 	    for (int c = 0; c < board[r].length; c ++){
-		if (board[r][c] = 0) ans += "__";
-	        if (board[r][c] < 10) ans += " _" + board[r][c];
+		if (board[r][c] == 0) ans += "__";
+	        if (board[r][c] <= 10) ans += " _" + board[r][c];
 		else ans += " " + board[r][c];
 	    }
 	}
+	return ans;
     }
 
     public boolean solve(int row, int col){
-	return solveH(row, col, 1);
+	return solveH(row-1, col-1, 1);
     }
 
     public boolean solveH(int row, int col, int level){
 	if (board[row][col] != 0) return false;
 	if (level > board.length * board[0].length) return true;
 	board[row][col] = level;
-	return solveH(row , col , level + 1) || solveH(row , col , level + 1) || solveH(row , col , level + 1) || solveH(row , col , level + 1) || solveH(row , col , level + 1) || solveH(row , col , level + 1) || solveH(row , col , level + 1) || solveH(row , col , level + 1)
+	return solveH(row + 1, col + 2, level + 1) || solveH(row + 1, col - 2, level + 1) || solveH(row - 1, col + 2, level + 1) || solveH(row + 2, col + 1, level + 1) || solveH(row + 2, col - 1, level + 1) || solveH(row - 2, col + 1, level + 1) || solveH(row - 2, col - 1, level + 1) || solveH(row - 1, col - 2, level + 1);
     }
 
-    public int countSolutions(){
+    // public int countSolutions(){
 
-    }
+    //}
 
     public static void main(String[] args){
-	new KnightBoard k = KnightBoard(8,8);
-	
+	KnightBoard k = new KnightBoard(8,8);
+	k.solve(1,1);
     }
     
 }
