131d278933c0206ac0ab2b9858b99b7e2cb5fa32
Vincent Lin <vlin@Vincents-Air.nyc.rr.com>
2018-04-09 00:15:45 -0400

    finished... i think

--- 08/MyLinkedList.java
+++ 08/MyLinkedList.java
@@ -21,6 +21,16 @@ public class MyLinkedList{
 	}
     }
 
+    public Node getNode(int index){
+	checkerror(index);
+	Node current = start;
+	for (int i = 0; i < index; i ++){
+	    current = current.getNext();
+	    
+	}
+	return current.getNext();
+    }
+
     public void clear(){
 	start = null;
 	end = null;
@@ -57,7 +67,8 @@ public class MyLinkedList{
 	return ans; 
     }
 
-    public void add(int index, Integer value){	
+    public void add(int index, Integer value){
+	checkerror(index);
 	Node current = end;
 	add(end.getValue());
 	for (int i = size - 2; i >= index; i --){
@@ -68,14 +79,17 @@ public class MyLinkedList{
 	size ++;
     }
 
-    /* public void add(int index, int Value){
+    public int indexOf(Integer value){
 	Node current = start;
-	add(0);
 	for (int i = 0; i < size; i ++){
-	    if (i == index)
+	    if (current.getValue() == value){
+		return i;
+	    }
+	    current = current.getNext();
 	}
-	}*/
-
+	return -1;
+    }
+    
     public boolean add(Integer value){
 	Node node = new Node(value);
 	node.setPrev(end);
@@ -84,18 +98,38 @@ public class MyLinkedList{
 	return true;
     }
 
-    public int remove(int index){
+    public Integer remove(int index){
+	checkerror(index);
 	size --;
 	Node current = start;
+	Integer ans;
 	for (int i = 0; i <= index; i ++){
-	    if (i == index) current.setValue(current.getNext());
+	    if (i == index){
+		current.setValue(current.getNext());
+		ans = current.getValue();
+	    }
 	    current = current.getNext();
 	}
 	for (int i = index; i < size; i ++){
 	    current.setValue(current.getNext().getValue());
 	    current = current.getNext();
 	}
-	end = end.get
+	end = end.get;
+	return ans;
     } 
+
+    public boolean remove(Integer value){
+	Node current = start;
+	for (int i = 0; i < size; i ++){
+	    if (current.getValue() == value){
+		size --;
+		remove(i);
+		return true;
+	    }
+	    current = current.getNext();
+	}
+	return false;rent.getNext();
+	}
+    }
     
 }
